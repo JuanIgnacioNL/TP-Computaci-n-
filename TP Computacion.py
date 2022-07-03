d7b3e9b39dfaf77df4e43caaa29d5238b000b99e
MSolPM <108634083+MSolPM@users.noreply.github.com>
2022-07-03 19:02:06 -0300

    Update TP Computacion.py

--- TP Computacion.py
+++ TP Computacion.py
@@ -19,6 +19,49 @@ def main() :
 
     elif opc ==2:
         dos()
+ import matplotlib.pyplot as plt
+import numpy as np
+
+fig, ax = plt.subplots()  #seria el 2A
+x = np.linspace(0, 2, 100) 
+ax.plot(x, x,"r", label='linea') 
+plt.xlabel("tiempo(dias)")
+plt.ylabel("costo(USD)")
+plt.title("Costo en base al tiempo")
+plt.legend(loc=2)
+
+
+fig, ax = plt.subplots()  #seria el 2B.
+x = np.linspace(0, 2, 100)
+ax.plot(x, x,"g", label='linea')  
+plt.xlabel("tiempo(dias)")
+plt.ylabel("riesgo(URA")
+plt.title("Riesgo en base al tiempo")
+plt.legend(loc=2)
+
+
+fig, ax = plt.subplots()  #seria el 2C
+x = np.linspace(0, 2, 100)
+ax.plot(x, x,"k", label='linea')  
+plt.xlabel("tiempo(dias)")
+plt.ylabel("riesgo y costo")
+plt.title("Velocidad")
+plt.legend(loc=2)
+
+
+np.random.seed(3)
+x = 3 + np.arange(2)
+y = np.random.uniform(2, 7, len(x))
+
+# plot
+fig, ax = plt.subplots()
+
+ax.bar(x, y, width=1, edgecolor="white", linewidth=0.7)
+
+ax.set(xlim=(0, 8), xticks=np.arange(1, 8),
+       ylim=(0, 8), yticks=np.arange(1, 8))
+
+plt.show()
         
 
     elif opc ==3:
